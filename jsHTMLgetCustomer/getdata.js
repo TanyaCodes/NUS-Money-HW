25c544f5ca768ed5bd2db49bf6a796ef7d08f728
Tanya <tanya.campos898@gmail.com>
2021-09-09 17:10:05 +0800

    test commit

--- jsHTMLgetCustomer/getdata.js
+++ jsHTMLgetCustomer/getdata.js
@@ -1,16 +1,17 @@
 function getFromServer(){
     $.getJSON('https://nusbackendstub.herokuapp.com/user/all', mydata );
     } 
-    // mydata is a callback function
+    // mydata is a callback function dicating how data will be presented
 
 
     function mydata(data){
       var text ="<ul>";
       data.forEach(function(item){    
       text = text + `<li> Account: ${item.user_id}, ${item.first_name} ${item.last_name}  <br> 
-                  Email: ${item.email} </li>`
+                  Email: ${item.email} </li>` 
       
       });
-      text += "</ul>"
-      $(".mypanel").html(text); //same as innerHTML
+      text += "</ul>"//same as innerHTML method but through string concatenation
+      $(".mypanel").html(text); //my panel is the indicated class of a div where the text will appear
+      //this is a jquery statement (as shown by $)
   }
